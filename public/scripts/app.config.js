8aad4c58f7369732fdd4f065d44ab6a2ef034087
PaulKatchmark <paulkatchmark@gmail.com>
2017-01-13 11:02:28 -0600

    fixed some files, deleted duplicate code

--- public/scripts/app.config.js
+++ public/scripts/app.config.js
@@ -12,22 +12,3 @@ app.config(function ($routeProvider, $locationProvider) {
 
   $locationProvider.html5Mode(true);
 });
-
-
-
-
-// angular.module('giphyApp')
-//        .config(function($routeProvider, $locationProvider) {
-//          $routeProvider.when('/', {
-//            templateUrl: 'views/home.html',
-//            controller: 'MainController as main',
-//          }).when('/home', {
-//            templateUrl: 'views/home.html',
-//            controller: 'MainController as main',
-//          }).when('/favorites', {
-//            templateUrl: 'views/favorites.html',
-//            controller: 'FavoriteController as faveCtrl',
-//          });
-//
-//          $locationProvider.html5Mode(true);
-//        });
